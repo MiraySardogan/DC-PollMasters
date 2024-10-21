e3f1b3d8529752f396860cde249c3aa516155aa0
Mir <raisulsust.bd@gmail.com>
2024-10-21 23:40:49 +0200

    started streamlit

---
+++ Mir/streamlit.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.write("hello")
+st.write("WElcome")
+
+st.image("Figure/2_likes_trump.png")
+
+
+# cd /d E:\ReDi_School\Data_Circle\Project\0_Github\Mir
+# activate my_env
+# streamlit run streamlit.py
+
+#We can choose my_env select interpreter here and run "streamlit run streamlit.py" directly.
