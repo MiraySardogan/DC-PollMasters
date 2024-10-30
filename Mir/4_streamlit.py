5110c9514d9708d03bbf44621a12f8eb49534e1d
Mir <raisulsust.bd@gmail.com>
2024-10-30 17:37:55 +0100

    added map to the dashboard

--- Mir/4_streamlit.py
+++ Mir/4_streamlit.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+import geopandas as gpd
 import altair as alt
 import plotly.express as px
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 #st.write("hello")
 #st.write("WElcome")
@@ -191,6 +195,99 @@ st.altair_chart(likes_chart, use_container_width=True)
 
 
 
+#Showing maps for twitter likes
+twitter= load_data()
+
+twitter_trump= twitter[twitter["candidate"]=="trump_df"]
+twitter_biden= twitter[twitter["candidate"]=="biden_df"]
+
+likes_trump= twitter_trump.groupby("state")["likes"].sum()
+likes_trump= pd.DataFrame(likes_trump).reset_index().rename(columns={"state": "name"}) #rename the column state as name
+
+likes_biden= twitter_biden.groupby("state")["likes"].sum()
+likes_biden= pd.DataFrame(likes_biden).reset_index().rename(columns={"state": "name"})
+
+# Load the USA states shapefile
+#usa_states = gpd.read_file("E:/ReDi_School/Data_Circle/Project/dataset/ne_110m_admin_1_states_provinces/ne_110m_admin_1_states_provinces.shp")
+usa_states = gpd.read_file("USA_shp_file/ne_110m_admin_1_states_provinces.shp")
+
+usa_trump_like= usa_states.merge(likes_trump, on="name", how= "inner")
+usa_biden_like= usa_states.merge(likes_biden, on="name", how= "inner")
+
+st.header("Comparing Likes between States of US")
+
+#st.map(usa_states)
+
+
+# Function to plot the shapefile
+
+#First visualize the USA map
+#def plot_map(usa_data, title, column):
+def plot_map(usa_data):
+    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    #usa_data.plot(column=column, ax=ax, legend=True, cmap='Blues', edgecolor='black')
+    #usa_data.plot(column=column, ax=ax, edgecolor='black')
+    usa_data.plot(ax=ax, facecolor= "white", edgecolor='black')
+    #ax.set_title(title)
+    ax.set_axis_off()
+    st.pyplot(fig)
+
+# Display map for Trump
+#plot_map(usa_states, "Likes for Trump", "likes")
+plot_map(usa_states)
+
+
+#Now plot the total likes as color ramp according to states both for Trump and Biden respectively
+fig, axes= plt.subplots(1,2)
+fig.set_size_inches(30,10)
+
+# Define discrete color levels (for example, from 0 to 700,000 in intervals of 100,000)
+#levels = np.arange(0, 700001, 100000)  # Adjust based on your data range
+
+#create a list for dataframes
+data_frames= [usa_trump_like, usa_biden_like]
+candidate= ["Trump", "Biden"]
+color_bars= ["YlOrBr", "Blues"]
+
+for index, ax in enumerate(axes.flat):
+    
+    # Define discrete color levels (for example, from 0 to 700,000 in intervals of 100,000)
+    levels = np.arange(0, data_frames[index]["likes"].max(), 100000)  # Adjust based on your data range
+
+    # Create a colormap and a corresponding norm for the levels
+    cmap = plt.get_cmap(color_bars[index])
+    norm = mcolors.BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+    
+    # Plot the USA with states
+    data_frames[index].plot(
+        ax=ax,
+        column="likes",    # Column to base the colors on
+        cmap=cmap,         # Use the defined colormap
+        norm=norm,         # Apply the discrete color normalization
+        edgecolor='black', # Add borderlines for states
+        legend=False        # Show legend
+    )
+
+    ax.set_title(f"Likes for {candidate[index]}", fontsize= 30, fontweight="bold") 
+
+    # Add a colorbar manually
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm._A = []  # Empty array for the ScalarMappable
+    cbar = fig.colorbar(sm, ax=ax, shrink= 0.9)
+    cbar.ax.set_title("Likes", fontsize= 30, fontweight="bold", pad=20)
+    cbar.ax.tick_params(labelsize=20)  # Adjust the labelsize as needed
+
+    #ax.set_axis_off()
+    # Remove tick labels
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+plt.tight_layout()
+st.pyplot(fig)
+plt.show()
+
+
+
 # Add filters in sidebar
 st.sidebar.header('Filters')
 selected_continent = st.sidebar.multiselect(
@@ -220,8 +317,3 @@ Key metrics include:
 # Footer
 st.sidebar.markdown('---')
 st.sidebar.markdown('Created with Streamlit')
-
-
-
-
-
